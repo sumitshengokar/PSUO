5f0140201a39eb6ef81982127a3f1c8455f2f30b
riotgage <rvsatwadhar@gmail.com>
2020-04-29 10:46:13 +0530

    Implemented complaints feature. Home page has two buttons. Need to add other buttons here only.

---
+++ Security/src/com/psou/HomePage.java
@@ -0,0 +1,88 @@
+package com.psou;//import statement
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+public class HomePage extends JFrame {
+
+    static HomePage frame;
+    JButton complaint_btn, exit_btn;
+    Color blue = new Color(51,122,183);
+    Color white = new Color(255, 255, 255);
+    JTextField t1;
+    JTextArea title, desc;
+    JLabel lb_PNR, lb_title, lb_desc;
+    private JPanel contentPane;
+
+    public static void main(String[] args)
+    {
+
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+
+                try
+                {
+                     frame = new HomePage();
+                    frame.setVisible(true);
+                    frame.setResizable(false);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    public HomePage()
+    {
+        setTitle("HomePage");
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 500, 500);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setBackground(white);
+        contentPane.setLayout(null);
+        contentPane.setAlignmentY(Component.CENTER_ALIGNMENT);
+        contentPane.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        complaint_btn = new JButton("Lodge A Complaint");
+        complaint_btn.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 24));
+        complaint_btn.setBounds( 100,150,294, 32);
+        complaint_btn.setBackground(blue);
+
+        contentPane.add(complaint_btn);
+
+
+        complaint_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                GrievanceLogin grievanceLogin=new GrievanceLogin();
+                grievanceLogin.setVisible(true);
+                frame.dispose();
+                setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            }
+        });
+
+        exit_btn = new JButton("Exit");
+        exit_btn.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 24));
+        exit_btn.setBounds( 100,300,294, 32);
+        exit_btn.setBackground(blue);
+        contentPane.add(exit_btn);
+
+        exit_btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+
+
+    }
+
+}
